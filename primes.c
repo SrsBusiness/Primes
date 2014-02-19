fd3038d6fd5509384e3c8b4f6cfb025b265d0340
SrsBusiness <dchenviolinist@gmail.com>
2014-02-19 03:26:49 +0000

    Started Huffman

--- primes.c
+++ primes.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include <assert.h>
 //#define
 
 // returns total number of primes in file
 char masks[] = {0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01};
 FILE *prime_check;
 int write_primes(long max, char *file){
+    // 1 means we're reading old primes, 0 means calculating new ones
+    char b;
     // to force alignment, make max the next multiple of 8;
     max += !!(0x7 & max) << 3;
     max &= ~(0x7);
@@ -23,6 +25,7 @@ int write_primes(long max, char *file){
         size = 0;
         write = fopen(file, "w+");
     }
+    b = size > 0;
     if(size >= max){
         printf("File already contains more than %d primes\n", max);
         return -1;
@@ -35,21 +38,27 @@ int write_primes(long max, char *file){
     unsigned long *old_primes = (long *)calloc(4096, sizeof(long));
     //int i; 
     unsigned long offset = 0; // offset in bits in read
-    int current_prime_old = 0; // current prime whose multiples to cross out 
-    int current_prime_new = 0;
+    unsigned int current_prime_old = 0; // index of current prime whose multiples to cross out 
+    unsigned long current_prime_new = 0;
     char current; // current byte in read
     //long mask = 0;
     int chunk_size = 0;
     while(1){ 
+        //printf("%d\n", 1);
         //printf("outer\n");
-        long p;
+        unsigned long p = 0;
         //printf("offset: %d, size: %d\n", offset, size);
-        if(offset < size || current_prime_old < chunk_size){
+        if(b){
+            //printf("old\n");
+            //printf("chunk_size before: %d\n", chunk_size);
+            //printf("current_prime_old before: %d\n", current_prime_old);
             //printf("old\n");
             if(!current_prime_old){
                 chunk_size = 0;
                 int j = 0;// # primes encountered so far
-                while(j < 4096 && offset < size){
+                //printf("offset: %d, size: %d, stop: %d\n", offset, size, stop);
+                while(j < 4096 && offset < size && offset <= stop){
+                    //printf("herp\n");
                     if(!(offset & 0x7)){
                         fread(&current, sizeof(char), 1, read);
                     }
@@ -59,13 +68,21 @@ int write_primes(long max, char *file){
                     }
                     offset++; 
                 }
-                //printf("altered offset: %d\n", offset);
             }
+            //printf("chunk_size after: %d\n", chunk_size);
+            //printf("current_prime_old after: %d\n", current_prime_old);
             // at this point prime chunk has been loaded
+            //printf("%d\n", 2);
             p = old_primes[current_prime_old];
-            current_prime_old = (current_prime_old + 1) % 4096;
+            if(chunk_size)
+                current_prime_old = (current_prime_old + 1) % chunk_size;
+            // if offset >= size b needs to be 0
+            // if offset > stop b needs to be 0
+            if(offset >= size || offset > stop && current_prime_old >= chunk_size)
+                b = 0;
         }else{
-            //printf("hi\n");
+            //printf("Shouldn't see this\n");
+            //printf("new\n");
             //printf("current_prime_new = %d\n", current_prime_new);
             while(new_primes[current_prime_new / 8] & 
                     masks[current_prime_new  % 8]){ 
@@ -74,9 +91,11 @@ int write_primes(long max, char *file){
                 //printf("p: %d\n", p);
                 current_prime_new++;
             }
+            //printf("%d\n", 3);
             p = current_prime_new + size;
             current_prime_new++;
         }
+        assert(p);
         if(p > stop){
             //printf("break_a\n");
             //printf("p: %d\n", p);
@@ -85,6 +104,7 @@ int write_primes(long max, char *file){
         //printf("P is %d\n", p);
         //printf("size = %d\n", size);
         for(unsigned long j = p * p; j < max; j += p){
+            //printf("%d\n", 4);
             //printf("j - size: %d\n", j - size);
             //printf("(j - size) % 8: %d\n", (j - size) % 8);
             if((long)(j - size) < 0){
@@ -124,6 +144,7 @@ int main(int argc, char **argv){
     unsigned long in = strtoul(argv[1], NULL, 10); 
     write_primes(in, argv[2]);
     printf("Write Finished.\n");
+    /*
     char buffer[20];
     printf("Enter file to scan: ");
     fflush(stdin);
@@ -134,4 +155,5 @@ int main(int argc, char **argv){
         scanf("%lu", &l);
         printf("Is %lu prime: %d\n", l, is_prime(l));
     }
+    */
 }
